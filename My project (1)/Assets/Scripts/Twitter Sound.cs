ed8f454d877ff55fff72f576d4953ed5a2b02982
LocalSnowi <michelle.sov04@gmail.com>
2026-04-14 13:33:31 -0400

    sound key works i guess

--- My project (1)/Assets/Scripts/Twitter Sound.cs
+++ My project (1)/Assets/Scripts/Twitter Sound.cs
@@ -1,16 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TwitterSound : MonoBehaviour
 {
+    public AudioSource sound;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        sound = GetComponent<AudioSource>();
+        sound.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       //can delete all the keyboard parts once we actually have a keyboard implemented
+        var kb = Keyboard.current;
+        if (kb == null) return;
+
+        if (kb.xKey.isPressed)
+        {
+            sound.Play();
+        } 
     }
 }
