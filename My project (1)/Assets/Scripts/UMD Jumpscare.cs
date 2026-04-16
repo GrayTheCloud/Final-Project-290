9f4a65e49447129070a7b47fb42eeaf16acedd95
LocalSnowi <michelle.sov04@gmail.com>
2026-04-16 13:04:57 -0400

    umd jumpscare complete

--- My project (1)/Assets/Scripts/UMD Jumpscare.cs
+++ My project (1)/Assets/Scripts/UMD Jumpscare.cs
@@ -1,16 +1,35 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class UMDJumpscare : MonoBehaviour
 {
+    public AudioSource sound;
+    public MeshRenderer tutel;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        tutel = GetComponent<MeshRenderer>();
+        sound = GetComponent<AudioSource>();
+        tutel.enabled = false;
+        sound.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Keyboard kb = Keyboard.current;
+        if (kb == null) return;
+
+        if (kb.uKey.isPressed)
+        {
+            tutel.enabled = true;
+            sound.Play();
+        }
+        else
+        {
+            tutel.enabled = false;
+        }
     }
 }
