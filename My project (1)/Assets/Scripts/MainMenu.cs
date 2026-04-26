1c7fa1006eb519cc7722303fdb69cb9f55cd0d37
Raymond Wu <rayzor851@gmail.com>
2026-04-26 01:06:24 -0400

    Audio 1 added to Home Screen

--- My project (1)/Assets/Scripts/MainMenu.cs
+++ My project (1)/Assets/Scripts/MainMenu.cs
@@ -3,13 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource audioSource;
+    void Start()
+    {
+        audioSource.Play();
+    }
     public void PlayGame()
     {
+        audioSource.Stop();
         SceneManager.LoadScene("Main Terrain");
     }
 
     public void QuitGame()
     {
+        audioSource.Stop();
         #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
         #else
